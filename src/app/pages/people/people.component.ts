36181d4a0456f7a2f19c7674e697f852e055020d
YAro <carlend94@ukr.net>
2018-05-28 18:33:50 +0300

    fixed wrong tegs

--- src/app/pages/people/people.component.ts
+++ src/app/pages/people/people.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-people',
   templateUrl: './people.component.html',
-  styleUrls: ['./people.component.css']
+  styleUrls: ['people.component.css']
 })
 export class PeopleComponent implements OnInit {
 
