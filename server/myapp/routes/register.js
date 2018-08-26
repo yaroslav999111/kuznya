5f09d30bff587cd7c2a495c510fd44a4606b7589
YAro <carlend94@ukr.net>
2018-08-26 18:04:57 +0300

    added servere

--- server/myapp/routes/register.js
+++ server/myapp/routes/register.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 var User = require('../models/user');
 
 /* GET users listing. */
@@ -12,18 +13,14 @@ router.post('/', function(req, res, next) {
         var sname = req.body.sname;
         var mobile = req.body.mobile;
         var password = req.body.password;
+        var email = req.body.email;
 
-        // var errors = req.validationErrors();
-
-
-    // if(errors) {
-    //     console.log('one field is empty')
-    // } else {
         var newUser = new User({
             username: username,
             sname: sname,
             mobile: mobile,
-            password: password
+            password: password,
+            email: email
         });
         User.createUser(newUser, function(err, user) {
             if(err) throw err;
